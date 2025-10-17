987590a288ba7347f08ba771a81324360be2fb63
Giang <trinhhoanggiang05@gmail.com>
2025-10-17 10:59:07 +0700

    remove unnecessary BrowserRouter in App.jsx (that's already in main.jsx)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,53 +1,60 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { useEffect } from 'react';
-import { useAuthStore } from './store/authStore';
-import ProtectedRoute from './components/ProtectedRoute';
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useAuthStore } from "./store/authStore";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-import LoginPage from './pages/LoginPage';
-import SignUpPage from './pages/SignUpPage';
-import HomePage from './pages/HomePage';
-import ProfilePage from './pages/ProfilePage';
-import SettingsPage from './pages/SettingsPage';
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import SettingsPage from "./pages/SettingsPage";
 
 function App() {
-  const { checkAuth, user } = useAuthStore();
+	const { checkAuth, user } = useAuthStore();
 
-  useEffect(() => {
-    checkAuth();
-  }, [checkAuth]);
+	useEffect(() => {
+		checkAuth();
+	}, [checkAuth]);
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route 
-          path="/login" 
-          element={user ? <Navigate to="/" /> : <LoginPage />} 
-        />
-        <Route 
-          path="/signup" 
-          element={user ? <Navigate to="/" /> : <SignUpPage />} 
-        />
-        
-        <Route path="/" element={
-          <ProtectedRoute>
-            <HomePage />
-          </ProtectedRoute>
-        } />
-        
-        <Route path="/profile" element={
-          <ProtectedRoute>
-            <ProfilePage />
-          </ProtectedRoute>
-        } />
-        
-        <Route path="/settings" element={
-          <ProtectedRoute>
-            <SettingsPage />
-          </ProtectedRoute>
-        } />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<Routes>
+			<Route
+				path="/login"
+				element={user ? <Navigate to="/" /> : <LoginPage />}
+			/>
+			<Route
+				path="/signup"
+				element={user ? <Navigate to="/" /> : <SignUpPage />}
+			/>
+
+			<Route
+				path="/"
+				element={
+					<ProtectedRoute>
+						<HomePage />
+					</ProtectedRoute>
+				}
+			/>
+
+			<Route
+				path="/profile"
+				element={
+					<ProtectedRoute>
+						<ProfilePage />
+					</ProtectedRoute>
+				}
+			/>
+
+			<Route
+				path="/settings"
+				element={
+					<ProtectedRoute>
+						<SettingsPage />
+					</ProtectedRoute>
+				}
+			/>
+		</Routes>
+	);
 }
 
 export default App;
