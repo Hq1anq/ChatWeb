4e2c5c4a7212b16be62e638d35e3cbba701f84e1
Hoang Hai <hoanghai12345k@gmail.com>
2025-11-02 22:22:45 +0700

    Sửa HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import Sidebar from '../components/Sidebar';
 import MessageContainer from '../components/MessageContainer';
+import { useAuthStore } from '../store/authStore';
 
 const HomePage = () => {
+  const { user } = useAuthStore(); // Lấy thông tin user đã đăng nhập
+
   return (
     <div className='flex h-screen w-screen overflow-hidden bg-base-100'>
-      {/* Sidebar - Chiếm 1/3 không gian */}
+      {/* Sidebar - Có thể truyền user xuống */}
       <div className='w-full sm:w-1/3 md:w-1/4 lg:w-1/5'>
-        <Sidebar />
+        <Sidebar user={user} />
       </div>
 
-      {/* Đường kẻ phân chia dọc (chỉ hiển thị trên màn hình sm trở lên) */}
       <div className="divider divider-horizontal hidden sm:flex m-0 p-0"></div>
 
-      {/* Khung tin nhắn - Chiếm phần còn lại */}
+      {/* MessageContainer - Có thể truyền user xuống */}
       <div className='grow h-full'>
-        <MessageContainer />
+        <MessageContainer user={user} />
       </div>
     </div>
   );
