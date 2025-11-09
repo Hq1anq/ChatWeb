dcf842cb8918eb9c2ce192de3e1f10635cac7c6a
Hoang Hai <hoanghai12345k@gmail.com>
2025-11-09 17:03:18 +0700

    Thêm trang 404 Not Found

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useAuthStore } from './store/authStore';
 import ProtectedRoute from './components/ProtectedRoute';
@@ -8,6 +8,7 @@ import SignUpPage from './pages/SignUpPage';
 import HomePage from './pages/HomePage';
 import ProfilePage from './pages/ProfilePage';
 import SettingsPage from './pages/SettingsPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   const { checkAuth, user } = useAuthStore();
@@ -17,36 +18,37 @@ function App() {
   }, [checkAuth]);
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route 
-          path="/login" 
-          element={user ? <Navigate to="/" /> : <LoginPage />} 
-        />
-        <Route 
-          path="/signup" 
-          element={user ? <Navigate to="/" /> : <SignUpPage />} 
-        />
-        
-        <Route path="/" element={
-          <ProtectedRoute>
-            <HomePage />
-          </ProtectedRoute>
-        } />
-        
-        <Route path="/profile" element={
-          <ProtectedRoute>
-            <ProfilePage />
-          </ProtectedRoute>
-        } />
-        
-        <Route path="/settings" element={
-          <ProtectedRoute>
-            <SettingsPage />
-          </ProtectedRoute>
-        } />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route 
+        path="/login" 
+        element={user ? <Navigate to="/" /> : <LoginPage />} 
+      />
+      <Route 
+        path="/signup" 
+        element={user ? <Navigate to="/" /> : <SignUpPage />} 
+      />
+      
+      <Route path="/" element={
+        <ProtectedRoute>
+          <HomePage />
+        </ProtectedRoute>
+      } />
+      
+      <Route path="/profile" element={
+        <ProtectedRoute>
+          <ProfilePage />
+        </ProtectedRoute>
+      } />
+      
+      <Route path="/settings" element={
+        <ProtectedRoute>
+          <SettingsPage />
+        </ProtectedRoute>
+      } />
+
+      {/* Route 404 */}
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 }
 
