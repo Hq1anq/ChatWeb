9e9bb6ab4908ea08cad98c38f77432608db6116d
Giang <trinhhoanggiang05@gmail.com>
2025-11-14 21:16:30 +0700

    setup socket.io-client

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,14 +1,18 @@
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
 import axiosInstance from '../lib/axios'
+import { toast } from 'react-hot-toast'
+import { io } from 'socket.io-client'
+
+const BASE_URL = 'http://localhost:5000'
 
 export const useAuthStore = create(
   persist(
-    (set) => ({
+    (set, get) => ({
       user: null,
-      // token: null,
       isLoading: false,
       error: null,
+      socket: null,
 
       // Đăng ký
       signup: async (data) => {
@@ -22,10 +26,7 @@ export const useAuthStore = create(
 
           set({ user: response.data })
 
-          // const { token, user } = response.data
-
-          // localStorage.setItem('token', token)
-          // set({ user, token, isLoading: false })
+          get().connectSocket()
 
           return { success: true }
         } catch (error) {
@@ -49,11 +50,9 @@ export const useAuthStore = create(
           })
 
           set({ user: response.data })
+          toast.success('Đăng nhập thành công!')
 
-          // const { token, user } = response.data
-
-          // localStorage.setItem('token', token)
-          // set({ user, token, isLoading: false })
+          get().connectSocket()
 
           return { success: true }
         } catch (error) {
@@ -71,26 +70,22 @@ export const useAuthStore = create(
       logout: async () => {
         try {
           await axiosInstance.post('/auth/logout')
+          get().disconnectSocket()
         } catch (error) {
           console.error('Logout error:', error)
         } finally {
-          // localStorage.removeItem('token')
-          // set({ user: null, token: null })
           set({ user: null })
         }
       },
 
       // Kiểm tra authentication
       checkAuth: async () => {
-        // const token = localStorage.getItem('token')
-        // if (!token) {
-        //   set({ user: null, token: null })
-        //   return false
-        // }
-
         try {
           const response = await axiosInstance.get('/auth/me')
           set({ user: response.data })
+
+          get().connectSocket()
+
           return true
         } catch (error) {
           // localStorage.removeItem('token')
@@ -102,6 +97,19 @@ export const useAuthStore = create(
 
       // Clear error
       clearError: () => set({ error: null }),
+
+      connectSocket: () => {
+        const { user } = get()
+        if (!user || get().socket?.connected) return
+
+        const socket = io(BASE_URL)
+        socket.connect()
+
+        set({ socket: socket })
+      },
+      disconnectSocket: () => {
+        if (get().socket?.connected) get().socket.disconnect()
+      },
     }),
     {
       name: 'auth-storage',
