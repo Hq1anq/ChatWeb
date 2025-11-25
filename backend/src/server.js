69b1c76d2c6b2b20949c5eea5603f8f5f5c9ab15
Giang <trinhhoanggiang05@gmail.com>
2025-11-25 14:08:35 +0700

    fix: remove dotenv, nodemon, add instanceName to db for express version

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
-import dotenv from 'dotenv' // 1. Import dotenv
-import path from 'path'     // 2. Import path
+import path from 'path' // 2. Import path
 
 // Import từ file socket của bạn
 import { app, server } from './lib/socket.js'
 
 import authRoutes from './route/auth.route.js'
 import messageRoutes from './route/message.route.js'
-
-// Kích hoạt dotenv để đọc file .env
-dotenv.config()
 
 const __dirname = path.resolve() // 4. Lấy đường dẫn gốc của dự án
 
@@ -22,7 +18,10 @@ app.use(cookieParser())
 // Cấu hình Static Files (Sửa lại cho chắc chắn)
 // Khi Frontend gọi: http://localhost:5000/profilepics/abc.jpg
 // Server sẽ tìm trong: /YourProject/public/profilepics/abc.jpg
-app.use('/profilepics', express.static(path.join(__dirname, 'public/profilepics')))
+app.use(
+  '/profilepics',
+  express.static(path.join(__dirname, 'public/profilepics'))
+)
 
 app.use(
   cors({
