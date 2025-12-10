75d5f8c0cd945a534c45ead7dfc23f25bcf9927a
Hoang Hai <hoanghai12345k@gmail.com>
2025-12-10 21:58:02 +0700

    update massages

--- frontend/src/components/chat/Messages.jsx
+++ frontend/src/components/chat/Messages.jsx
@@ -1,24 +1,47 @@
 import Message from './Message.jsx'
+import DateSeparator from './DateSeparator.jsx'
 import { useAuthStore } from '../../store/authStore'
-import { formatTime } from '../../lib/utils'
+import { formatTime, formatDateSeparator, getDateKey } from '../../lib/utils'
 
 const Messages = ({ messages }) => {
   const { user } = useAuthStore()
 
-  return (
-    <div className="flex flex-col gap-4">
-      {messages.map((msg) => (
+  // Hàm render messages với date separators
+  const renderMessagesWithDates = () => {
+    const elements = []
+    let lastDateKey = null
+
+    messages.forEach((msg) => {
+      const currentDateKey = getDateKey(msg.created)
+
+      // Nếu ngày khác với tin nhắn trước -> thêm separator
+      if (currentDateKey !== lastDateKey) {
+        elements.push(
+          <DateSeparator
+            key={`date-${currentDateKey}`}
+            date={formatDateSeparator(msg.created)}
+          />
+        )
+        lastDateKey = currentDateKey
+      }
+
+      // Thêm tin nhắn
+      elements.push(
         <Message
           key={msg.messageid}
           text={msg.content}
           file={msg.file}
           fromMe={msg.senderid === user.userid || msg.senderid === 'me'}
           time={formatTime(msg.created)}
-          isTemp={msg.isTemp} // Để hiển thị loading indicator
+          isTemp={msg.isTemp}
         />
-      ))}
-    </div>
-  )
+      )
+    })
+
+    return elements
+  }
+
+  return <div className="flex flex-col gap-2">{renderMessagesWithDates()}</div>
 }
 
 export default Messages
