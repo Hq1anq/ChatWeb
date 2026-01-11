c696bf24042331512eb37ae975c40e378898cdff
Hq1anq <trinhhoanggiang05@gmail.com>
2026-01-11 15:36:12 +0700

    fix date time (UCT in ubuntu sqlserver), not tracking dockẻ-compose.yaml

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,10 +1,13 @@
 // Format thời gian HH:MM
 export const formatTime = (timestamp) => {
   const date = new Date(timestamp)
-  const hours = date.getUTCHours().toString().padStart(2, '0')
-  const minutes = date.getUTCMinutes().toString().padStart(2, '0')
+
+  const hours = date.getHours().toString().padStart(2, '0')
+  const minutes = date.getMinutes().toString().padStart(2, '0')
+
   return `${hours}:${minutes}`
 }
+
 
 export function formatTimeAgo(isoUTCString) {
   if (!isoUTCString) return '' // No timestamp
